464bea8a6d389ed5f52e727ea5a2155d15f09891
katjes <katjes733@gmx.net>
2026-01-18 11:49:17 -0700

    chore: only include relevant files

--- build/src/components/ChatbotError/ChatbotError.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './ChatbotError.css';
-interface IChatbotErrorProps {
-    message: string;
-}
-declare const ChatbotError: ({ message }: IChatbotErrorProps) => React.JSX.Element;
-export default ChatbotError;
--- build/src/components/ChatbotMessage/ChatBotMessageAvatar/ChatbotMessageAvatar.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-declare const ChatbotMessageAvatar: () => React.JSX.Element;
-export default ChatbotMessageAvatar;
